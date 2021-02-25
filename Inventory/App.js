36905cfe3fdb870eef4c33f8b50b56beedb1a2b7
shreejithora <dorsh78@gmail.com>
2021-02-25 13:38:23 +0545

    Drawer added

--- Inventory/App.js
+++ Inventory/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import HomeTabNav from './navigation/HomeTabNav';
+import DrawerContent from './screens/Drawer/DrawerContent';
+
+const Drawer = createDrawerNavigator();
+
 const App = () => {
   return(
-    <View style={styles.container}>
-      <Text>Welcome to Inventory</Text>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator drawerContent={ props => <DrawerContent {...props}/>}>
+        <Drawer.Screen name="Home" component = { HomeTabNav } />
+        {/* <Drawer.Screen name="Users" component = { UsersNav } />
+        <Drawer.Screen name="Traders" component = { TradersNav } />
+        <Drawer.Screen name="Products" component = { ProductsNav } />
+        <Drawer.Screen name="Sales" component = { SalesNav } />
+        <Drawer.Screen name="Entries" component = { EntriesNav } />
+        <Drawer.Screen name="Quotation" component = { QuotationNav } /> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
   )
 }
 
